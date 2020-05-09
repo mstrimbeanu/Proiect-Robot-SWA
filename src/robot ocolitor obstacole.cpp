ed2f048f42ef3d5d374fa03fffab576b672f04e5
mstrimbeanu <m.strimbeanu@gmail.com>
2020-05-09 18:26:13 +0300

    robot ocolitor obstacole final
    modificare digitalwrite in analorwrite, pt a putea modifica viteza, schimbarea pinilor, si modificarea codului pt a functiona

--- src/robot ocolitor obstacole.cpp
+++ src/robot ocolitor obstacole.cpp
@@ -1,69 +1,59 @@
 #include <Servo.h>
+Servo servoMotor;
 
-//definire senzor distanta
-#define ECHO_PIN A4
-#define TRIG_PIN A5
+int Echo = A3;  
+int Trig = A2; 
+int vitezaSunet = 342;
+long durata;
+float distanta;
 
 //definire motoare
 #define a1 5
 #define a2 10
 #define b1 6
 #define b2 11
-
-
+#define vitezaMasina 150
 //definire pin servomotor
-#define PIN_Servo 9
-//servomotor
-Servo servoMotor;
+#define PIN_Servo 12
 
-int stangaDistanta = 0, dreaptaDistanta = 0, inainteDistanta = 0;
-int Trig;
-int Echo;
-int vitezaSunet = 342;
-long durata;
-float distanta;
+int stangaDistanta = 0, dreaptaDistanta = 0, inainteDistanta = 0;;
 
-void inainte()
-{
-        digitalWrite(a1,HIGH);
+void inainte(){ 
+        analogWrite(a1,vitezaMasina);
         digitalWrite(a2,LOW);
         digitalWrite(b1,LOW);
-        digitalWrite(b2,HIGH);
+        analogWrite(b2,vitezaMasina);
 }
 
-void inapoi()
-{
+void inapoi() {
         digitalWrite(a1,LOW);
-        digitalWrite(a2,HIGH);
-        digitalWrite(b1,HIGH);
+        analogWrite(a2,vitezaMasina);
+        analogWrite(b1,vitezaMasina);
         digitalWrite(b2,LOW);
 }
 
-void dreapta()
-{
+void stanga() {
+        analogWrite(a1,vitezaMasina);
+        digitalWrite(a2,LOW);
+        digitalWrite(b1,LOW);
+        digitalWrite(b2,LOW); 
+}
+
+void dreapta() {
         digitalWrite(a1,LOW);
         digitalWrite(a2,LOW);
         digitalWrite(b1,LOW);
-        digitalWrite(b2,HIGH);
+        analogWrite(b2,vitezaMasina);
 }
 
-void stanga()
-{
-        digitalWrite(a1,HIGH);
-        digitalWrite(a2,LOW);
-        digitalWrite(b1,LOW);
-        digitalWrite(b2,LOW);
-}
-//functia stop pentru ca masina sa fie orpita cand incepem testarea
-void stop()
-{
+void stop() {
          digitalWrite(a1,LOW);
          digitalWrite(a2,LOW);
          digitalWrite(b1,LOW);
          digitalWrite(b2,LOW);
-}
+} 
 
-// test de masurare snezor ultrasonic
+//functie testare distanta 
 int testDistanta()
 {
   digitalWrite(Trig, LOW);
@@ -74,63 +64,59 @@ int testDistanta()
   durata = pulseIn (Echo ,HIGH);
   distanta = (durata * 0.0342) / 2; //transformare in cm
   return distanta;
-}
+} 
 
-void setup() {
-  // put your setup code here, to run once:
-  //servo motor
-  servoMotor.attach(9);
-  // senzor ultrasunete
-  pinMode(Echo, INPUT);
-  pinMode(Trig , OUTPUT); 
-  //motoare 
+void setup() { 
+  servoMotor.attach(12);  
+  Serial.begin(9600);     
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT);  
   pinMode(b1, OUTPUT);
   pinMode(b2, OUTPUT);
   pinMode(a1, OUTPUT);
   pinMode(a2, OUTPUT);
   stop();
-}
+} 
 
-void loop() {
-  // put your main code here, to run repeatedly:
- 
-  servoMotor.write(90); //initializarea servo inainte
-  delay(500);
-  inainteDistanta = testDistanta();
+void loop() { 
+    servoMotor.write(90);  //pozitionare servo motor in fata
+    delay(500); 
+    inainteDistanta = testDistanta();
 
-  if(inainteDistanta <= 10)
-  {
-    stop();
-    delay(500);
-    servoMotor.write(0);
-    delay(1000);
-    dreaptaDistanta = testDistanta();
-    delay(500);
-    servoMotor.write(180);
-    delay(1000);
-    stangaDistanta = testDistanta();
-    //mutam servo la 90 grade, inainte
-    delay(500);
-    servoMotor.write(90);
-
-    if(dreaptaDistanta > stangaDistanta)
-    {
-      dreapta(); 
-      delay(360);
-    }
-    else if(dreaptaDistanta < stangaDistanta)
-    {
-      stanga(); 
-      delay(360);
-    }
-    else if((stangaDistanta <=10 || dreaptaDistanta <= 10))
-    {
-      inapoi(); 
-      delay(180);
-    }
-    else
-    {
-      inainte();
-    }
-  }
+    if(inainteDistanta <= 40) {     
+      stop();
+      delay(500);                         
+      servoMotor.write(10);          
+      delay(1000);      
+      dreaptaDistanta = testDistanta();
+      
+     // delay(500);
+     // servoMotor.write(90);              
+      delay(1000);                                                  
+      servoMotor.write(180);              
+      delay(1000); 
+      stangaDistanta = testDistanta();
+      
+      delay(500);
+      servoMotor.write(90);              
+      delay(1000);
+      if(dreaptaDistanta > stangaDistanta) {
+        dreapta();
+        delay(360);
+      }
+      else if(dreaptaDistanta < stangaDistanta) {
+        stanga();
+        delay(360);
+      }
+      else if((dreaptaDistanta <= 40) || (stangaDistanta <= 40)) {
+        inapoi();
+        delay(180);
+      }
+      else {
+        inainte();
+      }
+    }  
+    else {
+        inainte();
+    }                     
 }
